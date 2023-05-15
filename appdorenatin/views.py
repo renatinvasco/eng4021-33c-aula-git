579674032cba74c5075dc3c9c2ef573addbd8e40
renatinvasco <132673129+renatinvasco@users.noreply.github.com>
2023-05-15 20:45:14 -0300

    Update views.py

--- appdorenatin/views.py
+++ appdorenatin/views.py
@@ -3,12 +3,14 @@ from .models import Nacional
 from .models import Artilheiro
 
 def home(request):
+  #manda para o html principal as informações do BD do artilheiros e do nacional para serem postos nas ul e ol
   nacional = Nacional.objects.all()
   artilheiro = Artilheiro.objects.all()
   context = { "nacional": nacional , "artilheiro": artilheiro }
   return render(request, "RenatoRibeiro.html", context=context)
 
 def create_artilheiro(request):
+  # Para se criar o artilheiro em um formulario as informações e se essas forem trazidas como POST se registra as informações
   if request.method == "POST":
     Artilheiro.objects.create(
       nome=request.POST["Nome"],
@@ -21,6 +23,7 @@ def create_artilheiro(request):
   return render(request, "artilheiro_form.html")
 
 def create_nacional(request):
+  # Para se criar o Titulo nacional em um formulario as informações e se essas forem trazidas como POST se registra as informações
   if request.method == "POST":
     Nacional.objects.create(
       title=request.POST["Title"],
@@ -33,6 +36,7 @@ def create_nacional(request):
   return render(request, "nacional_form.html")
 
 def update_artilheiro(request, artilheiro_id):
+  # Função para mudar informações do banco de dados e acessar o dado por meio da ID, podendo ir em determinado dado
   artilheiro = Artilheiro.objects.get(id=artilheiro_id)
   if request.method == "POST":
     artilheiro.nome = request.POST["Nome"]
@@ -48,6 +52,7 @@ def update_artilheiro(request, artilheiro_id):
   return render(request, "artilheiro_form.html", context={"artilheiro": artilheiro})
 
 def update_nacional(request, nacional_id):
+  # Função para mudar informações do banco de dados e acessar o dado por meio da ID, podendo ir em determinado dado
   nacional = Nacional.objects.get(id=nacional_id)
   if request.method == "POST":
     nacional.title = request.POST["Title"]
@@ -63,6 +68,7 @@ def update_nacional(request, nacional_id):
   return render(request, "nacional_form.html", context={"nacional": nacional})
 
 def delete_artilheiro(request, artilheiro_id):
+  # Função para deletar informações do banco de dados e acessar o dado por meio da ID, podendo ir em determinado dado e deleta-lo
     artilheiro = Artilheiro.objects.get(id=artilheiro_id)
     if request.method == "POST":
       if "confirma" in request.POST:
@@ -72,6 +78,7 @@ def delete_artilheiro(request, artilheiro_id):
     return render(request, "delete_artilheiro.html", context={"artilheiro": artilheiro})
 
 def delete_nacional(request, nacional_id):
+   # Função para deletar informações do banco de dados e acessar o dado por meio da ID, podendo ir em determinado dado e deleta-lo
     nacional = Nacional.objects.get(id=nacional_id)
     if request.method == "POST":
       if "confirma" in request.POST:
