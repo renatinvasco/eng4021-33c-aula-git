0130c2cb83eb8dba4aa044c264bdde8c4229a8c2
renatinvasco <132673129+renatinvasco@users.noreply.github.com>
2023-05-15 20:55:33 -0300

    Update admin.py

--- appdorenatin/admin.py
+++ appdorenatin/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Nacional
 from .models import Artilheiro
 
+# registra no admin os models e suas informações para serem criados lá por meio do login (usuário do createsuperuser)
 admin.site.register(Nacional)
 admin.site.register(Artilheiro)
 # Register your models here.
